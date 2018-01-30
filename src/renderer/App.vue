40518ec7e209899de2a6d43df5a58ddd6cf3e993
Yifei Chu <yifei.chu@d5techs.com>
2018-01-30 14:48:47 +0800

    init project

---
+++ src/renderer/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nk-conference'
+  }
+</script>
+
+<style>
+  /* CSS */
+  @import url('/static/css/font.css');
+
+  body {
+    font-family: 'Source Han Sans', Arial;
+    font-weight: 300;
+  }
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="wrapper">
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <main>
+      <div class="left-side">
+        <span class="title">
+          欢迎使用NK Conference
+        </span>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landing-page',
+    methods: {
+    }
+  }
+</script>
+
+<style>
+</style>
